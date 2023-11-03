ffcc0eba4ca38fa698ea7977c7abdc0d1c74ff3a
atikurahad <atikurahad@gmail.com>
2023-11-04 00:07:33 +0600

    header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from './components/Header';
 
 const App = () => {
   return (
@@ -6,6 +7,7 @@ const App = () => {
       <h1>
         Hello 
       </h1>
+      <Header />
     </div>
   );
 }
