6af801a189f69af484b7841f75b69d41c1c0dfd7
atikurahad <atikurahad@gmail.com>
2023-11-05 18:30:09 +0600

    navbar add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Header from './components/Header';
-
+import './index.css'
 const App = () => {
   return (
     <div>
-      <h1>
+      <h1 className='text-6xl'>
         Hello 
       </h1>
       <Header />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
-    return (
-        <div>
-            <h1> I am From Header</h1>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h1> I am From Header</h1>
+       
+      <div className="d-flex">
+      <p>Home</p>
+      <p>Service</p>
+      <p>About</p>
+      <p>Project</p>
+      <p>Blog</p>
+      <p>Contact</p>
+     
+      </div>
+      
+    </div>
+  );
+};
 
 export default Header;
